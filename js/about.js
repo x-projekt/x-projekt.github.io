9de91eea01a7d896fde04532203bd1ee1c8f64f9
Harsha Vardhan <x.projekt@outlook.com>
2021-02-20 19:40:08 +0530

    Add about.js for on-hover transition

---
+++ js/about.js
@@ -0,0 +1,7 @@
+function hover(element) {
+    element.setAttribute('src', "./img/youtube_monochrome_mark/youtube_social_circle_red.png");
+}
+  
+function unhover(element) {
+    element.setAttribute('src', "./img/youtube_monochrome_mark/youtube_social_circle_dark.png");
+}
