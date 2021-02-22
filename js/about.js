bc5b471cb9c8b96fb69e58b91b1e67c68b3e5e0f
Harsha Vardhan <x.projekt@outlook.com>
2021-02-23 00:36:27 +0530

    Move hover-effect to CSS and drop JS script

--- js/about.js
+++
@@ -1,7 +0,0 @@
-function hover(element) {
-    element.setAttribute('src', "./img/youtube_monochrome_mark/youtube_social_circle_red.png");
-}
-  
-function unhover(element) {
-    element.setAttribute('src', "./img/youtube_monochrome_mark/youtube_social_circle_dark.png");
-}
