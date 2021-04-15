81a33daff402646439abeea6d20de9db030d7007
Harsha Vardhan <x.projekt@outlook.com>
2021-04-15 17:09:02 +0530

    Update script for dynamic project listing

--- js/projects.js
+++ js/projects.js
@@ -1,51 +1,17 @@
 "use strict";
 
-let json = `{
-    "placeholders": {
-        "title": "{project.title}"
-    },
+const contentSelector = ".container.content";
 
-    "projects": [
-        {
-            "card.image": {
-                "data": "./img/svg/sensor.svg"
-            },
-            "card.title": {
-                "text": "Dual Raspberry Pi Stereo Vision",
-                "href": "./projects/dual-raspi-stereo-vision.html"
-            },
-            "card.description": "A stereo-vision system composed of two Raspberry Pi's each with an infrared camera module. The images are processed using the OpenCV library while a custom software is used for synchronizing the two cameras.",
-            "card.tags": ["python", "computer-vision"],
-            "card.date": {
-                "to": "2016-06-20",
-                "from": "2017-04-20"
-            }
-        },
-        {
-            "card.image": {
-                "data": "./img/svg/visual-effects.svg"
-            },
-            "card.title": {
-                "text": "Product design and visual effects",
-                "href": "./projects/product-design-vfx.html"
-            },
-            "card.description": "This details the products I designed and their associated product design videos I created.",
-            "card.tags": ["solidworks", "after-effects", "3ds-max", "illustrator"],
-            "card.date": {
-                "to": "2017-05-20",
-                "from": "2013-03-20"
-            }
-        }
-    ]
-}`;
+function showError() {
+    $(contentSelector).html(errorFragment());
+}
 
-let projects = JSON.parse(json)["projects"];
-let p;
-let projectList = "";
-
-for (let i = 0; i < projects.length; i++) {
-    p = projects[i];
-    projectList += `<div class="card-object flex-md-row flex-column">
+function showProjects(projectList) {
+    let p;
+    let container = $(contentSelector);
+    for (let i = 0; i < projectList.length; i++) {
+        p = projectList[i];
+        container.append(`<div class="card-object flex-md-row flex-column">
             <object class="card-image" type="image/svg+xml" data="${p["card.image"]["data"]}">
                 <img src="./img/fallback-image.png" height="200" width="200">
             </object>
@@ -62,12 +28,26 @@ for (let i = 0; i < projects.length; i++) {
                 <div class="card-footing">
                     <div class="d-inline-flex flex-wrap w-auto">
                         ${p["card.tags"].map(function (key) {
-                            return `<div class="hashtags">#${key}</div>`
-                        }).join("\n")}
+            return `<div class="hashtags">#${key}</div>`
+        }).join("\n")}
                     </div>
                     <div class="card-date">${printDate(parseDate(p["card.date"]["from"]))} - ${printDate(parseDate(p["card.date"]["to"]))}</div>
                 </div>
             </div>
-        </div>`;
+        </div>`);
+    }
 }
-document.querySelector(".container.content").innerHTML += projectList;
+
+$(document).ready(function() {
+    const url = "https://github.com/harshatech2012/harshatech2012.github.io/blob/iss2/projects/project-list.json";
+    fetch(url, {method: "GET"}).then(response => {
+        if (response.ok) {
+            let projectsJson = response.json();
+            showProjects(projectsJson["projects"])
+        } else {
+            showError();
+        }
+    }).catch(error => {
+        console.log(error);
+    });
+});
