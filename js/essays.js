7e2581d1febdb63107d7b2dc63e99b731bbd7ada
Harsha Vardhan <x.projekt@outlook.com>
2022-11-19 14:37:47 +0530

    feat: create js/essays.js stub file

---
+++ js/essays.js
@@ -0,0 +1,51 @@
+"use strict";
+
+function showProjects(data) {
+    let container = $(".container.essays-insert");
+    if (data.hasOwnProperty("essays")) {
+        data.projects.forEach(p => {
+            container.append(
+                `<div class="card-object flex-md-row flex-column">
+                    <object class="card-image" type="image/svg+xml" data="${p["card.image"]["data"]}">
+                        <img src="./img/fallback-image.png" height="200" width="200">
+                    </object>
+                    <div class="card-content">
+                        <div class="card-heading">
+                            <a href="${p["card.title"]["href"]}">
+                                ${p["card.title"]["text"]}
+                            </a>
+                        </div>
+                        <p class="lead">
+                            ${p["card.description"]}
+                        </p>
+                        <hr class="w-100 my-0">
+                        <div class="card-footing">
+                            <div class="d-inline-flex flex-wrap w-auto">
+                                ${p["card.tags"].map(function (key) {
+                                    return `<div class="hashtags">#${key}</div>`
+                                }).join("\n")}
+                            </div>
+                            <div class="card-date">${printDate(parseDate(p["card.date"]["from"]))} - ${(p["card.date"]["to"] === "present") ? "present" : printDate(parseDate(p["card.date"]["to"]))}</div>
+                        </div>
+                    </div>
+                </div>`);
+        });
+    } else {
+        insertError("Missing JSON property: 'projects' property not found in project-list.json");
+    }
+}
+
+$(document).ready(function() {
+    const url = "../essays/essay-list.json";
+    fetch(url, {method: "GET"})
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                insertError("Unable to fetch project-list.json");
+            }
+        }).then(showProjects)
+        .catch(error => {
+            insertError(error);
+        });
+});
